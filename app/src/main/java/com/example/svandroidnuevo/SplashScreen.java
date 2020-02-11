b80f7a0e7cec72aa91c716bff9f4bd78915bbd52
Noelia Perez <noepf_29@hotmail.com>
2020-02-11 12:47:30 +0100

    Funciona todo

--- app/src/main/java/com/example/svandroidnuevo/SplashScreen.java
+++ app/src/main/java/com/example/svandroidnuevo/SplashScreen.java
@@ -2,7 +2,9 @@ package com.example.svandroidnuevo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        //Para que aparezca la pantalla 3 seg y luego se ejecute el main
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
     }
 }
